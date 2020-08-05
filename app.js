84a1695ae4bc5d39d514a235152c32575eeaf35b
loona <r.arswendo.r@gmail.com>
2020-08-05 17:08:09 +0700

    Initial commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ dotenv.config();
 connection.on("open", () => {
   server.listen("10", "0.0.0.0", () => {
     if (server.listening) {
-      console.log("Server is listening");
+      console.log("Server is listening on port: " + process.env.APP_PORT);
     }
   });
 });
