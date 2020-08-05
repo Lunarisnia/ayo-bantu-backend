e8bde899b16528f18d97462c0151f1a15bd8c2bb
loona <r.arswendo.r@gmail.com>
2020-08-05 17:11:24 +0700

    Initial commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const connection = require("./config/dbCon");
 dotenv.config();
 
 connection.on("open", () => {
-  server.listen("10", "0.0.0.0", () => {
+  server.listen(process.env.APP_PORT, "0.0.0.0", () => {
     if (server.listening) {
       console.log("Server is listening on port: " + process.env.APP_PORT);
     }
